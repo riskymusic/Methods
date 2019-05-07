248ff9a39bd6ca34958496bd1d254137049be433
riskymusic <48032117+riskymusic@users.noreply.github.com>
2019-05-08 11:36:51 +1200

    methods

---
+++ Array/Array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+     
+        
+       
+        
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Here's Method to find out your BMI");
+            Console.Write("Please enter your height in metres: ");
+            //optain height from user 
+            double h = double.Parse(Console.ReadLine());
+            Console.Write("please enter your weight in kg's: ");
+            //obtain weight from user 
+            double w = double.Parse(Console.ReadLine());
+            double result = BMI(h, w);
+            Console.WriteLine($"your BMI is:  {Task2(result)}");
+            //display result
+            float catagory = float.Parse(Console.ReadLine());
+        }
+        static double BMI(double w, double h)
+        {
+            return w / (h * h);
+        }
+
+        static string Task2(double bmi)
+
+        {
+            if (bmi >= 30)
+                return "your Obese";
+
+            else if (bmi >= 25 && bmi < 30)
+
+                return "you need to loose some weight";
+
+            else if (bmi >= 18.5 && bmi < 25)
+
+                return "Normal weight";
+            else
+                return "You are underweight";
+        }
+
+  
+        
+    
+    }
+}
---
+++ Classes/classes2/classes2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Tesla";
+            car1.Model = "Model Y";
+            car1.Colour = "red";
+            car1.Rego = "ABC123";
+
+            Console.WriteLine($"Car details: \nMake: {car1.Make}\nModel: {car1.Model}\nColour: {car1.Colour}\nRego: {car1.Rego}");
+        }
+    }
+
+    class Car
+    { 
+        //Attributes//
+        private string model;
+        private string make;
+        private string rego;
+        private string colour;
+        //Getters and Setters//
+        public string Model { get { return model; } set { model = value; } }
+        public string Make { get { return make; } set { make = value; } }
+        public string Rego { get { return rego; } set { rego = value; } }
+        public string Colour { get { return colour; } set { colour = value; } }
+    }
+}
+
+
