31c620ef4fdd3dcd0ba24e56e7b0fff1b6ae814a
riskymusic <48032117+riskymusic@users.noreply.github.com>
2019-05-16 13:48:39 +1200

    Methods

---
+++ Rounding/Rounding & Switch statement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******TASK3*******");
+            Console.WriteLine("Please enter what number your transaction ends with between 1 - 9 :  ");
+            int userNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{userNum} gets rounded = {Rounding(userNum)}");
+
+            Console.ReadLine();
+        
+
+        }
+        
+        public  static  string Rounding (int num)
+        {
+            string result = "-1";
+            switch(num)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    result = "down";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    result = "up";
+                    break;
+            }
+
+            return result;
+
+        }
+         
+    
+    }
+
+}
